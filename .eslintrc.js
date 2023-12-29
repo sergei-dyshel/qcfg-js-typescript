87dac8807e4e6162c219d030e3147f7878563fea
Sergei Dyshel <qyron.private@gmail.com>
2023-12-29 16:32:56 +0530

    +

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+/**
+ * @type {import("eslint").Linter.Config}
+ */
+module.exports = {
+  "extends": [
+    "@sergei-dyshel"
+  ],
+parser: '@typescript-eslint/parser',
+	parserOptions: {
+		tsconfigRootDir: __dirname,
+		project: ['./tsconfig.json'],
+	},
+}
