aa2422c15141463d6265a2479f7a02822f7efeb8
Sergei Dyshel <qyron.private@gmail.com>
2024-01-04 18:03:38 +0530

    +

--- src/string.ts
+++ src/string.ts
@@ -1,1 +1,8 @@
 export * from "case-anything";
+
+export function removePrefix(str: string, prefix: string): string {
+  if (str.startsWith(prefix)) {
+    return str.slice(prefix.length);
+  }
+  return str;
+}
