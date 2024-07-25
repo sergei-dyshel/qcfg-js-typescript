0a27edb5d65c93cf06c10812a3412ae7d6d39c46
Sergei Dyshel <sergei@dyshel.com>
2024-07-25 16:12:55 +0300

    SetAdapter: add stubs for new ES2024 Set methods

--- src/set.ts
+++ src/set.ts
@@ -1,3 +1,20 @@
+// Copied from lib.esnext.collection.d.ts to resolve TS errors
+interface ReadonlySetLike<T> {
+  /**
+   * Despite its name, returns an iterator of the values in the set-like.
+   */
+  keys(): Iterator<T>;
+  /**
+   * @returns A boolean indicating whether an element with the specified value exists in the
+   *   set-like or not.
+   */
+  has(value: T): boolean;
+  /**
+   * @returns The number of (unique) elements in the set-like.
+   */
+  readonly size: number;
+}
+
 /**
  * Allows mapping from arbitrary type by providing an adapter - function that converts from given
  * key type to key type that works with {@link Map}
@@ -53,5 +70,35 @@ export class SetAdapter<T, A> implements Set<T> {
 
   get [Symbol.toStringTag](): string {
     return new Set(this.values())[Symbol.toStringTag];
+  }
+
+  // see definitions in lib.esnext.collection.d.ts
+
+  union<U>(_other: ReadonlySetLike<U>): Set<T | U> {
+    throw new Error("Operation not supported");
+  }
+
+  intersection<U>(_other: ReadonlySetLike<U>): Set<T & U> {
+    throw new Error("Operation not supported");
+  }
+
+  difference<U>(_other: ReadonlySetLike<U>): Set<T> {
+    throw new Error("Operation not supported");
+  }
+
+  symmetricDifference<U>(_other: ReadonlySetLike<U>): Set<T | U> {
+    throw new Error("Operation not supported");
+  }
+
+  isSubsetOf(_other: ReadonlySetLike<unknown>): boolean {
+    throw new Error("Operation not supported");
+  }
+
+  isSupersetOf(_other: ReadonlySetLike<unknown>): boolean {
+    throw new Error("Operation not supported");
+  }
+
+  isDisjointFrom(_other: ReadonlySetLike<unknown>): boolean {
+    throw new Error("Operation not supported");
   }
 }
