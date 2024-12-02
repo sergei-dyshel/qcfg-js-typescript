5b720f2e4933a29721ff95b1fc6e303fcde14670
Sergei Dyshel <sergei@dyshel.com>
2024-12-03 00:30:32 +0200

    Add jsonDeepEqual

--- src/json.ts
+++ src/json.ts
@@ -2,3 +2,7 @@ import jsonStableStringify from "json-stable-stringify";
 import * as jsoncParser from "jsonc-parser";
 
 export { jsonStableStringify, jsoncParser };
+
+export function jsonDeepEqual(actual: unknown, expected: unknown) {
+  return jsonStableStringify(actual) === jsonStableStringify(expected);
+}
