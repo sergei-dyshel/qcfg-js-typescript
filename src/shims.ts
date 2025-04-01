68e0d2cb60d46852c2ede161e1421102408b9099
Sergei Dyshel <sergei@dyshel.com>
2025-04-02 00:03:59 +0300

    add esNext set.prototype method shims

--- src/shims.ts
+++ src/shims.ts
@@ -1,1 +1,10 @@
 import "disposablestack/auto";
+
+// remove after upgradeing to node v22
+import "set.prototype.difference/auto";
+import "set.prototype.intersection/auto";
+import "set.prototype.isdisjointfrom/auto";
+import "set.prototype.issubsetof/auto";
+import "set.prototype.issupersetof/auto";
+import "set.prototype.symmetricdifference/auto";
+import "set.prototype.union/auto";
