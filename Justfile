d5c2d4f72dfc2f9fcf22836ef7d990bb265485ec
Sergei Dyshel <sergei@amazon.com>
2025-10-27 23:22:23 -0700

    json: add ajv package

--- Justfile
+++ Justfile
@@ -4,6 +4,8 @@ import? './node_modules/@sergei-dyshel/eslint-config/export.just'
 import? '../../node_modules/@sergei-dyshel/prettier-config/export.just'
 import? './node_modules/@sergei-dyshel/prettier-config/export.just'
 
+import? '../../export.just'
+
 import './export.just'
 
 _default:
--- src/json.ts
+++ src/json.ts
@@ -1,7 +1,9 @@
+import * as ajv from "ajv";
 import jsonStableStringify from "json-stable-stringify";
 import * as jsoncParser from "jsonc-parser";
 
-export { jsonStableStringify, jsoncParser };
+export { Ajv } from "ajv";
+export { ajv, jsonStableStringify, jsoncParser };
 
 export function jsonDeepEqual(actual: unknown, expected: unknown) {
   return jsonStableStringify(actual) === jsonStableStringify(expected);
