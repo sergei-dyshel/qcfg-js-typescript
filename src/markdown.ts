c6e4ae633b6f05972059d52caa15e07616130a68
Sergei Dyshel <sergei@amazon.com>
2026-03-27 13:23:49 -0700

    markdown: add plaintify and remove-markdown deps

--- src/markdown.ts
+++ src/markdown.ts
@@ -1,13 +1,23 @@
 import * as marked from "marked";
+import markedPlaintify from "marked-plaintify";
 import type { UriLike } from "./uri";
+// (presumably) faster alternative to plaintify (doesn't strip [//] comments though)
+export { default as strip } from "remove-markdown";
 
-export function render(markdown: string) {
-  const html = marked.marked.parse(markdown, { gfm: true });
+export interface RenderOptions {
+  plaintify?: boolean;
+}
+
+export function render(markdown: string, options?: RenderOptions) {
+  const html = createRenderer(options).parse(markdown, {
+    gfm: true,
+    async: false,
+  });
   return html;
 }
 
-export function inline(markdown: string) {
-  const html = marked.marked.parseInline(markdown, { gfm: true });
+export function inline(markdown: string, options?: RenderOptions) {
+  const html = createRenderer(options).parseInline(markdown, { gfm: true, async: false });
   return html;
 }
 
@@ -15,4 +25,12 @@ export const HORIZONTAL_LINE = "\n\n---\n\n";
 
 export function link(text: string, uri: UriLike) {
   return `[${text}](${uri.toString()})`;
+}
+
+function createRenderer(options?: RenderOptions) {
+  const renderer = new marked.Marked({ gfm: true });
+  if (options?.plaintify) {
+    renderer.use(markedPlaintify());
+  }
+  return renderer;
 }
